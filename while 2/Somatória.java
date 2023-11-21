0ed4ec703e009aad51626602eaf752fa10d5319e
GrazielleEvSantos <142915017+GrazielleEvSantos@users.noreply.github.com>
2023-11-20 22:32:55 -0300

    Create Somatória.java

---
+++ while 2/Somatória.java
@@ -0,0 +1,21 @@
+package laçoRepetição1;
+
+
+public class Somatória {
+
+	public static void main(String[] args) {
+		int i = 1, soma = 0;
+		
+		while (i <= 100) {
+			soma = soma + i;
+			i++; //i=1+1
+			
+			
+			
+		}
+		
+		System.out.println("Somatória é igual a: "+soma);
+	
+	}
+
+}
