6477928f9ffc031c2829455c04c8fe2d875f50b1
GrazielleEvSantos <142915017+GrazielleEvSantos@users.noreply.github.com>
2023-11-20 22:15:52 -0300

    Create Idade.java

---
+++ else if 2/Idade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Idade {
+	public static void main(String[]args) {
+		Scanner in = new Scanner(System.in);
+		 int idade, anoNasc, anoAtual;
+		 
+		 System.out.println("Entre com seu ano de nascimento");
+		 anoNasc = in.nextInt();
+		 System.out.println("Entre com o ano atual");
+		 anoAtual = in.nextInt();
+		 
+		 idade = anoAtual-anoNasc;
+		 
+		 if(idade <10) {
+			 System.out.println(idade+" Crian�a");
+			 
+		 }
+		 else if(idade >=10 && idade <18) {
+			 System.out.println(idade+" Adolescente");
+		 }
+		 else if(idade>=18 && idade<60) {
+			 System.out.println(idade+" Adulto");
+		 }
+		 else if(idade >=60) {
+			 System.out.println(idade+" Idoso");
+		 }
+		 }
+	}
