50826ebd474cf4227b01d0019e6c5da9a368005d
GrazielleEvSantos <142915017+GrazielleEvSantos@users.noreply.github.com>
2023-11-20 22:16:14 -0300

    Create Media.java

---
+++ else if 2/Media.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Media {
+	public static void main(String[]args) {
+		Scanner in = new Scanner(System.in);
+		
+		double n1, n2, media, exame, notaExame;
+		
+		System.out.println("Digite sua primeria nota");
+		n1 = in.nextDouble();
+		System.out.println("Digite sua segunda nota");
+		n2 = in.nextDouble();
+		media = (n1+n2)/2;
+		
+		if(media<3) {
+			System.out.println("Reprovado");
+		}
+		else if(media>=6) {
+			System.out.println("Aprovado");
+		}
+		else if (media >=3 && media <6){
+			System.out.println("Voc� est� em exame. Digite sua nota do exame");
+			exame = in.nextDouble();
+			notaExame = (media+exame)/2;
+			
+			if(notaExame>=6) {
+				System.out.println("Aprovado");
+			}
+			else{
+				System.out.println("Reprovado");
+			}
+		}
+	}
+}
